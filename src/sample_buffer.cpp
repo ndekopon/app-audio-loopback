6332b62452ae0a68fac1e34fd281681019e742bf
ndekopon <92087784+ndekopon@users.noreply.github.com>
2024-04-03 11:17:37 +0900

    バッファを1秒から10秒に変更

--- src/sample_buffer.cpp
+++ src/sample_buffer.cpp
@@ -4,8 +4,9 @@
 
 namespace
 {
-	constexpr UINT32 COMMON_LOWER = app::COMMON_SAMPLES / 4 * 1;
-	constexpr UINT32 COMMON_HIGHER = app::COMMON_SAMPLES / 4 * 3;
+	constexpr UINT32 BUFFER_SECONDS = 10;
+	constexpr UINT32 COMMON_LOWER = app::COMMON_SAMPLES * BUFFER_SECONDS / 4 * 1;
+	constexpr UINT32 COMMON_HIGHER = app::COMMON_SAMPLES * BUFFER_SECONDS / 4 * 3;
 
 	bool lesser(uint64_t _a, uint64_t _b)
 	{
@@ -25,16 +26,15 @@ namespace
 namespace app
 {
 	sample_buffer::sample_buffer(UINT32 _duplicate_threshold, UINT32 _threashold_interval)
-		: buffer_(static_cast<size_t>(COMMON_SAMPLES * COMMON_BYTES_PERFRAME), 0)
+		: buffer_(static_cast<size_t>(COMMON_SAMPLES * BUFFER_SECONDS * COMMON_BYTES_PERFRAME), 0)
 		, last_set_(0)
-		, last_get_(COMMON_SAMPLES - (COMMON_SAMPLES / 1000) * 50) // 初回は50ms遅延入れておく
+		, last_get_(COMMON_SAMPLES * BUFFER_SECONDS - (COMMON_SAMPLES / 1000) * 100) // 初回は100ms遅延入れておく
 		, skip_count_(0)
 		, duplicate_count_(0)
 		, skip_threshold_(_duplicate_threshold + _threashold_interval)
 		, duplicate_threshold_(_duplicate_threshold)
 		, set_count_(0)
 		, get_count_(0)
-		, manual_skip_(0)
 	{
 	}
 
@@ -44,11 +44,12 @@ namespace app
 
 	void sample_buffer::set(const BYTE* _data, UINT32 _frames)
 	{
+		const auto buffer_frames = COMMON_SAMPLES * BUFFER_SECONDS;
 		auto m = last_set_;
 
-		if (m + _frames > COMMON_SAMPLES)
+		if (m + _frames > buffer_frames)
 		{
-			auto f2 = (m + _frames) - COMMON_SAMPLES;
+			auto f2 = (m + _frames) - buffer_frames;
 			auto f1 = _frames - f2;
 			std::memcpy(&buffer_.at(m * COMMON_BYTES_PERFRAME), _data +  0 * COMMON_BYTES_PERFRAME, f1 * COMMON_BYTES_PERFRAME);
 			std::memcpy(&buffer_.at(0 * COMMON_BYTES_PERFRAME), _data + f1 * COMMON_BYTES_PERFRAME, f2 * COMMON_BYTES_PERFRAME);
@@ -60,49 +61,40 @@ namespace app
 			last_set_ = m + _frames;
 		}
 		set_count_ += _frames;
-		last_set_ %= COMMON_SAMPLES;
+		last_set_ %= buffer_frames;
 	}
 
 	void sample_buffer::get(BYTE* _data, UINT32 _frames)
 	{
+		const auto buffer_frames = COMMON_SAMPLES * BUFFER_SECONDS;
 		auto m = last_get_;
 		auto w = last_set_;
-		auto wl = (w + COMMON_SAMPLES - (COMMON_SAMPLES / 1000) * skip_threshold_) % COMMON_SAMPLES;
-		auto wg = (w + COMMON_SAMPLES - (COMMON_SAMPLES / 1000) * duplicate_threshold_) % COMMON_SAMPLES;
-		if (lesser((m + _frames) % COMMON_SAMPLES, wl))
+
+		// しきい値によるスキップ処理
+		auto wl = (w + buffer_frames - (COMMON_SAMPLES / 1000) * skip_threshold_) % buffer_frames;
+		auto wg = (w + buffer_frames - (COMMON_SAMPLES / 1000) * duplicate_threshold_) % buffer_frames;
+		if (lesser((m + _frames) % buffer_frames, wl))
 		{
 			m += 1;
-			m %= COMMON_SAMPLES;
+			m %= buffer_frames;
 			skip_count_++;
 			get_count_ += _frames + 1;
 		}
-		else if (greater((m + _frames) % COMMON_SAMPLES, wg))
+		else if (greater((m + _frames) % buffer_frames, wg))
 		{
-			m += COMMON_SAMPLES - 1;
-			m %= COMMON_SAMPLES;
+			m += buffer_frames - 1;
+			m %= buffer_frames;
 			duplicate_count_++;
-			get_count_ += _frames - 1;
+			get_count_ += (_frames - 1);
 		}
 		else
 		{
-			// 500ms -> 1skip
-			auto skip = get_count_ / (COMMON_SAMPLES / 2);
-			if (skip_count_ > 0 && skip != manual_skip_)
-			{
-				m += 1;
-				m %= COMMON_SAMPLES;
-				manual_skip_ = skip;
-				get_count_ += _frames - 1;
-			}
-			else
-			{
-				get_count_ += _frames;
-			}
+			get_count_ += _frames;
 		}
 
-		if (m + _frames > COMMON_SAMPLES)
+		if (m + _frames > buffer_frames)
 		{
-			auto f2 = (m + _frames) - COMMON_SAMPLES;
+			auto f2 = (m + _frames) - buffer_frames;
 			auto f1 = _frames - f2;
 			std::memcpy(_data +  0 * COMMON_BYTES_PERFRAME, &buffer_.at(m * COMMON_BYTES_PERFRAME), f1 * COMMON_BYTES_PERFRAME);
 			std::memcpy(_data + f1 * COMMON_BYTES_PERFRAME, &buffer_.at(0 * COMMON_BYTES_PERFRAME), f2 * COMMON_BYTES_PERFRAME);
@@ -113,7 +105,7 @@ namespace app
 			std::memcpy(_data, &buffer_.at(m * COMMON_BYTES_PERFRAME), _frames * COMMON_BYTES_PERFRAME);
 			last_get_ = m + _frames;
 		}
-		last_get_ %= COMMON_SAMPLES;
+		last_get_ %= buffer_frames;
 	}
 
 	UINT64 sample_buffer::get_skip_count()
